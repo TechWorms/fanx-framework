826c22f91c9aa6379390d6fae7438d2481d6f9fd
Remisor <remus.copil@voicemailtel.com>
2014-11-25 03:42:34 +0000

    Base1

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,5 @@
-if ENV['capy'] == 'true'
+# Headless env and required gems *
+if ENV['headless'] == 'true'
 require 'rubygems'
 require 'capybara'
 require 'capybara/cucumber'
@@ -10,14 +11,21 @@ require 'mail'
 require 'capybara-screenshot/cucumber'
 
 
-
+# Capybara Session Configuration *
 
 Capybara::Session.new(:webkit)
+Capybara.run_server = false
+Capybara.default_wait_time = 30
+
+
+# Capybara Screenshot Module *
+
 Capybara::Screenshot.webkit_options = { width: 1024, height: 768 }
 Capybara::Screenshot.prune_strategy = :keep_last_run
 Capybara.save_and_open_page_path = "/home/fanx-wat/reports/screens"
-Capybara.run_server = false
-Capybara.default_wait_time = 30
+
+
+# Capybara Ignore SSL errors module *
 
 Capybara.register_driver :webkit_ignore_ssl do |app|
   browser = Capybara::Webkit::Browser.new(Capybara::Webkit::Connection.new).tap do |browser|
@@ -28,7 +36,10 @@ end
 Capybara.javascript_driver = :webkit_ignore_ssl
 
 
+# Standard url fetch (implement curl header response in future release) *
+
 def url
   URI.parse(current_url)
 end
+
 end
